ef6bf478790ecb5b86daaebfac546e8f72254a69
clue31415 <clue31415@gmail.com>
2024-10-27 22:34:46 +0900

    Update home.js

--- src/home.js
+++ src/home.js
@@ -15,11 +15,13 @@ export default function Home() {
     .then(data => {
       console.log(data);
       console.log('success');
+if data{
 const _inputData = data.products.map((rowData)=> ({
   name: rowData[1],
   title: rowData[2],
   content: rowData[3]
 }))
+}
 setInputData(inputData.concat(_inputData))
 })
 .catch(rejected => {
