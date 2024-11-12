8ffac2c51204494a49dc5b7b8bf45409bf2e78f8
clue31415 <clue31415@gmail.com>
2024-11-12 17:55:29 +0900

    Update home.js

--- src/home.js
+++ src/home.js
@@ -54,7 +54,7 @@ console.log(post_title);
         })}
       </div>
           <button onClick={()=>{if (page>1){ changepage(page + -1);}}}>&lt;</button>{page}<button onClick={()=>{if (page<100){ changepage(page + 1);}}}>&gt;</button>
-      <p></p>
+      <style>p{margin_bottom: 40px;}</style>
       <p></p>
     </div>
   );
